66fdcafe007129d69721deaedb30fc787c24f15e
Paris Tyus <Tyus.Paris1@gmail.com>
2020-04-29 11:08:49 -0500

    added comments

--- codecademy.py
+++ codecademy.py
@@ -1,3 +1,5 @@
+# sale exercise #
+
 lovely_loveseat_description = """Lovely Loveseat. Tufted polyester blend on wood. 
 32 inches high X 40 inches wide x 30 inches deep. Red or White"""
 
@@ -26,50 +28,66 @@ print("Customer One Items")
 print(customer_one_itemization)
 print("Customer One Total")
 print(customer_one_tax)
+# ---------------------------------------- #
 
-# equations exercise  #
+
+# physics --- force & work  #
 train_mass = 22680
 train_acceleration = 10
 train_distance = 100
 
 bomb_mass = 1
 
+
 def get_force(mass, acceleration):
     return mass * acceleration
-train_force= (get_force(train_mass,train_acceleration))
+
+
+train_force = (get_force(train_mass, train_acceleration))
 print(train_force)
-print("The GE train supplies " + str (train_force)+ " Newtons of force")
+print("The GE train supplies " + str(train_force) + " Newtons of force")
 
-def get_energy(mass,c = 3*10**8):
-    return mass * c**2
-bomb_energy= get_energy(bomb_mass)
-print (bomb_energy)
-print("The 1Kg bomb supplies "+ str (bomb_energy) + " Joules")
 
-def f_to_c(f_temp, c_temp):
-    c_temp = (f_temp - 32 * 5/9)
-    return c_temp
+def get_energy(mass, c=3 * 10 ** 8):
+    return mass * c ** 2
 
-f100_in_celsius = f_to_c
-f_temp = 100
 
-print(f_to_c(100,0))
+bomb_energy = get_energy(bomb_mass)
+print(bomb_energy)
+print("The 1Kg bomb supplies " + str(bomb_energy) + " Joules")
 
-def c_to_f(c_temp,f_temp):
-    f_temp = (c_temp *(9/5) + 32)
-    return f_temp
 
-c0_in_fahrenhiet = c_to_f
-c_temp = 0
-
-print(c_to_f(0,100))
-
-def get_work(mass,acceleration,distance):
+def get_work(mass, acceleration, distance):
     get_force = mass * acceleration
     result = get_force * distance
     return result
 
-train_work = get_work(train_mass,train_acceleration,train_distance)
-print(train_work)
-print("The GE train does " +str (train_work) + " Joules of work over" + str(train_distance)+ " meters")
 
+train_work = get_work(train_mass, train_acceleration, train_distance)
+print(train_work)
+print("The GE train does " + str(train_work) + " Joules of work over" + str(train_distance) + " meters")
+
+
+# ---------------------------------------------------- #
+
+# Temperature conversion #
+def f_to_c(f_temp, c_temp):
+    c_temp = (f_temp - 32 * 5 / 9)
+    return c_temp
+
+
+f100_in_celsius = f_to_c
+f_temp = 100
+
+print(f_to_c(100, 0))
+
+
+def c_to_f(c_temp, f_temp):
+    f_temp = (c_temp * (9 / 5) + 32)
+    return f_temp
+
+
+c0_in_fahrenhiet = c_to_f
+c_temp = 0
+
+print(c_to_f(0, 100))
