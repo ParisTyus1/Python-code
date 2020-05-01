b4ad156b240d63f63451983309aa84c09d2775be
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-01 09:50:28 -0500

    conditionals and/or statements

--- codecademy.py
+++ codecademy.py
@@ -143,3 +143,19 @@ statement_two = (4 * 2 <= 8) and (7 - 1 == 6)
 def graduation_reqs(gpa, credits):
     if credits >= 120 and gpa >= 2.0:
         return "You meet the requirements to graduate!"
+
+# The registrars office at Calvin Coolidge’s Cool College has another request.
+# They want to send out a mailer with information on the commencement
+# ceremonies to students who have met at least one requirement for
+# graduation (120 credits and 2.0 GPA).
+# Write a function called graduation_mailer that takes two inputs,
+# gpa and credits and checks if a student either has
+# 120 or more credits or a GPA 2.0 or higher and if so returns True.
+
+statement_one = (2 - 1 > 3) or (-5 * 2 == -10)
+
+statement_two = (9 + 5 <= 15) or (7 != 4 + 3)
+
+def graduation_mailer(gpa,credits):
+    if credits >= 120 or gpa >= 2.0:
+        return True
