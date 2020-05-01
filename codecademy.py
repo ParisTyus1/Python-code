88d68eac53711398828fcf23268546a3fc1bd618
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-01 10:20:38 -0500

    operands

--- codecademy.py
+++ codecademy.py
@@ -159,3 +159,33 @@ statement_two = (9 + 5 <= 15) or (7 != 4 + 3)
 def graduation_mailer(gpa,credits):
     if credits >= 120 or gpa >= 2.0:
         return True
+
+# The registrar’s office at Calvin Coolidge’s Cool College has been so
+# impressed with your work so far that they have another task for you.
+# They want you to return to the first function you wrote,
+# graduation_reqs, and add in several checks using and and not statements.
+
+# If a student meets both requirements the function should return
+# "You meet the requirements to graduate!"
+# If a student’s GPA is greater or equal to 2.0 but they don’t have enough credits the function should return
+# "You do not have enough credits to graduate."
+# If they have enough credits but their GPA is less than 2.0 the function should return
+# "Your GPA is not high enough to graduate."
+# If they do not have enough credits and their GPA is less than 2.0, the function should return
+# "You do not meet either requirement to graduate!"
+# Make sure your return value matches those strings exactly.
+# Capitalization, punctuation, and spaces matter!
+
+statement_one = False
+
+statement_two = True
+
+def graduation_reqs(gpa, credits):
+    if (gpa >= 2.0) and (credits >= 120):
+        return "You meet the requirements to graduate!"
+    if (gpa >= 2.0) and not (credits >= 120):
+        return "You do not have enough credits to graduate."
+    if not (gpa >= 2.0) and (credits >= 120):
+        return "Your GPA is not high enough to graduate."
+    if not (gpa >= 2.0) and not (credits >= 120):
+        return "You do not meet either requirement to graduate!"
