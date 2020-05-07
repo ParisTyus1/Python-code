db5f095c879c3732ca6ef03fb9b75ff559f596e4
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-07 11:37:26 -0500

    more code from mosh tutorial. key value pairing

--- app.py
+++ app.py
@@ -1,55 +1,55 @@
-import math
-
-print(math.ceil(3.9))
-print(math.floor(2.9))
-
-print("Paris Tyus")
-print('o-hello-there-you-')
-print(' |Im |learning |python|')
-print('*' * 10)
-price = 10
-rating = 5.2
-name = 'Tyus'
-is_published = False
-print(price)
-print(name)
-
-full_name = 'John Smith'
-age = 20
-is_new = True
-
-name = input('What is your name? ')
-print('Hi ' + name)
-color = input('What is your favorite color? ')
-print('Hi ' + name + ', your favorite color is ' + color + '.')
-
-birth_year = input('Birth year: ')
-age = 2019 - int(birth_year)
-print(age)
-
-course = 'python for beginners'
-another = course[:]
-print(another)
-
-name = 'jennifer'
-print(name[1:-1])
-
-first = 'john'
-last = 'smith'
-message = first + last + ' is a coder'
-msg = f'{first} {last} is a coder'
-print(msg)
-
-course = 'python for beginners'
-print(len(course))
-print(course.upper())
-print(course.title())
-print(course.lower())
-print(course.find('p'))
-print(course.replace('beginners', 'Advanced'))
-print('python' in course)
-
-print(10 / 3)
+# import math
+#
+# print(math.ceil(3.9))
+# print(math.floor(2.9))
+#
+# print("Paris Tyus")
+# print('o-hello-there-you-')
+# print(' |Im |learning |python|')
+# print('*' * 10)
+# price = 10
+# rating = 5.2
+# name = 'Tyus'
+# is_published = False
+# print(price)
+# print(name)
+#
+# full_name = 'John Smith'
+# age = 20
+# is_new = True
+#
+# name = input('What is your name? ')
+# print('Hi ' + name)
+# color = input('What is your favorite color? ')
+# print('Hi ' + name + ', your favorite color is ' + color + '.')
+#
+# birth_year = input('Birth year: ')
+# age = 2019 - int(birth_year)
+# print(age)
+#
+# course = 'python for beginners'
+# another = course[:]
+# print(another)
+#
+# name = 'jennifer'
+# print(name[1:-1])
+#
+# first = 'john'
+# last = 'smith'
+# message = first + last + ' is a coder'
+# msg = f'{first} {last} is a coder'
+# print(msg)
+#
+# course = 'python for beginners'
+# print(len(course))
+# print(course.upper())
+# print(course.title())
+# print(course.lower())
+# print(course.find('p'))
+# print(course.replace('beginners', 'Advanced'))
+# print('python' in course)
+#
+# print(10 / 3)
 # print(10 // 3)
 # print(10 % 3)
 # print(10 * 3)
@@ -60,6 +60,20 @@ print(10 / 3)
 # x -= 3
 # print(x)
 
-x = 2.9
-print(round(x))
-print(abs(x))
+# x = 2.9
+# print(round(x))
+# print(abs(x))
+
+
+# key, value pairing#
+phone = input("Phone: ")
+digits_mapping = {
+    "1":"One",
+    "2":"Two",
+    "3":"Three",
+    "4":"Four"
+}
+output = ""
+for ch in phone:
+    output += digits_mapping.get(ch, "!") + ""
+print(output)
