00818d3702f24cf7e2b33775bc98fd8dbe9c3a31
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-10 14:46:17 -0500

    maria web design exercise

--- codecademy.py
+++ codecademy.py
@@ -411,3 +411,35 @@ list3 = range(5, 15, 3)
 # Ends before 40
 
 list5 = range(0,40,5)
+
+# Maria's web design
+# Maria is entering customer data for her web design business. You’re going to help her organize her data.
+
+# Start by turning this list of customer first names into a list called first_names. Make sure to enter the names in this order:
+
+# Ainsley
+# Ben
+# Chani
+# Depak
+
+first_names = ['Ainsley', 'Ben', 'Chani', 'Depak']
+
+# Create an empty list called age.
+age = []
+
+# Depak’s age is 42. Use .append() to add 42 to age.
+age.append(42)
+
+
+# Maria needs a list of the ages for all customers. Create a new list called all_ages that adds age with the following list, containing the ages for Ainsley, Ben, and Chani:
+
+# [32, 41, 29]
+# Make sure all_ages begins with the ages for Ainsley, Ben, and Chani and ends with Depak’s age (stored in age)!
+all_ages = [32, 41, 29] + age
+
+# Create a new variable called name_and_age that combines first_names and all_ages using zip.
+
+name_and_age= zip(first_names, all_ages)
+
+# Create a range object called ids with an id number for each customer. Since there are 4 customers, so id values should go from 0 to 3.
+ids = range(0,4,3)
