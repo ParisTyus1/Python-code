2c094f8ad82cecc69ddafc3cfe67ecb9967f7e25
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-10 13:18:35 -0500

    manipulating lists

--- codecademy.py
+++ codecademy.py
@@ -370,4 +370,12 @@ is using premium ground shipping and it will cost """ )
 
 print(premium_ground_shipping)
 
+# //////////////////manipulating list //////////////////////
+names = ['Jenny', 'Alexus', 'Sam', 'Grace']
+dogs_names = ['Elphonse', 'Dr. Doggy DDS', 'Carter', 'Ralph']
 
+names_and_dogs_names = zip(names, dogs_names)
+
+list_of_names_and_dogs_names = list(names_and_dogs_names)
+
+print(list(list_of_names_and_dogs_names))
