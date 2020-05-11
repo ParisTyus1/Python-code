92f833945811032cdba7b24244ec86eec293c61b
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-11 13:00:53 -0500

    sorting

--- codecademy.py
+++ codecademy.py
@@ -589,3 +589,13 @@ games = ['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
 
 games_sorted = sorted(games)
 print(games_sorted)
+
+inventory = ['twin bed', 'twin bed', 'headboard', 'queen bed', 'king bed', 'dresser', 'dresser', 'table', 'table', 'nightstand', 'nightstand', 'king bed', 'king bed', 'twin bed', 'twin bed', 'sheets', 'sheets', 'pillow', 'pillow']
+
+inventory_len= len(inventory)
+first = inventory[0]
+last = inventory[-1]
+inventory_2_6 = inventory[2:6]
+first_3 = inventory[0:3]
+twin_beds = inventory.count('twin bed')
+inventory.sort()
