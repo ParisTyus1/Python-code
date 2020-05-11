39adfb2c2d0eec0def8c4f7ccb53aa34f9d9854a
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-11 12:44:20 -0500

    sorting

--- codecademy.py
+++ codecademy.py
@@ -563,3 +563,29 @@ votes = ['Jake', 'Jake', 'Laurie', 'Laurie', 'Laurie', 'Jake', 'Jake',
 jake_votes = votes.count('Jake')
 
 print(jake_votes)
+
+### Exercise 1 & 2 ###
+addresses = ['221 B Baker St.', '42 Wallaby Way', '12 Grimmauld Place', '742 Evergreen Terrace', '1600 Pennsylvania Ave', '10 Downing St.']
+
+# Sort addresses here:
+addresses.sort()
+print(addresses)
+
+### Exercise 3 ###
+names = ['Ron', 'Hermione', 'Harry', 'Albus', 'Sirius']
+names.sort()
+
+### Exercise 4 ###
+cities = ['London', 'Paris', 'Rome', 'Los Angeles', 'New York']
+
+sorted_cities = cities.sort()
+print(sorted_cities)
+
+games = ['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
+
+#1. Use sorted to order games and create a new list called games_sorted.
+
+#2. Use print to inspect games and games_sorted. How are they different?
+
+games_sorted = sorted(games)
+print(games_sorted)
