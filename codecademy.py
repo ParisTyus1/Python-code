09db389a7da9ac604aa2c88c295b586dc07aa91b
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-11 09:13:35 -0500

    finished manipulating list

--- codecademy.py
+++ codecademy.py
@@ -443,3 +443,39 @@ name_and_age= zip(first_names, all_ages)
 
 # Create a range object called ids with an id number for each customer. Since there are 4 customers, so id values should go from 0 to 3.
 ids = range(0,4,3)
+
+# ///////////////////python gradebook//////////////////////
+
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+# Create a list called subjects and fill it with the classes you are taking:
+
+# "physics"
+# "calculus"
+# "poetry"
+# "history"
+subjects = ['physics', 'calculus', 'poetry', 'history']
+subjects.append('computer science')
+# Create a list called grades and fill it with your scores:
+
+# 98
+# 97
+# 85
+# 88
+grades = [98, 97, 85, 88, 100]
+
+# Use the zip() function to combine subjects and grades.
+
+# Save this zip object as a list into a variable called gradebook.
+gradebook= list(zip(subjects, grades))
+
+# Your grade for visual arts just came in! You got a 93!
+# After the creation of gradebook (but before you print it out),
+# use append to add ("visual arts", 93) to gradebook.
+gradebook.append(('visual arts', 93))
+print(gradebook)
+
+# You also have your grades from last semester, stored in last_semester_gradebook.
+# Create a new variable full_gradebook with the items from
+# both gradebook and last_semester_gradebook.
+full_gradebook = gradebook + last_semester_gradebook
+print(full_gradebook)
