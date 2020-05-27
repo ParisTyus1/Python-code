51f39b4eaf937a92f0ecc10cbeb5d322bd35983d
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-27 11:55:03 -0500

    while loops

--- codecademy.py
+++ codecademy.py
@@ -680,3 +680,13 @@ for i in ages:
     if i < 21:
         continue
     print(i)
+
+# /////////////////While loops////////////
+all_students = ["Alex", "Briana", "Cheri", "Daniele", "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"]
+students_in_poetry = []
+
+while len(students_in_poetry) < 6:
+    student = all_students.pop()
+    students_in_poetry.append(student)
+
+print(students_in_poetry)
