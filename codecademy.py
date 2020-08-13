559c77b8617b4f4f0e25900d2c917244e5455afd
Paris Tyus <Tyus.Paris1@gmail.com>
2020-08-13 14:58:12 -0500

    string manipultaion review

--- codecademy.py
+++ codecademy.py
@@ -882,3 +882,28 @@ def reversed_list(lst1, lst2):
 #Uncomment the lines below when your function is done
 print(reversed_list([1, 2, 3], [3, 2, 1]))
 print(reversed_list([1, 5, 3], [3, 2, 1]))
+
+first_name = "Julie"
+last_name = "Blevins"
+
+# Working on string review
+
+# Copeland’s Corporate Company has realized that their policy
+# of using the first five letters of an employee’s last name
+# as a user name isn’t ideal when they have multiple employees
+# with the same last name.
+
+# Write a function called account_generator that takes two inputs,
+# first_name and last_name and concatenates the first three
+# letters of each and then returns the new account name.
+
+first_name = "Julie"
+last_name = "Blevins"
+
+def account_generator(first_name, last_name):
+    account_name = first_name[:3] + last_name[:3]
+    return account_name
+
+new_account = account_generator(first_name, last_name)
+
+print(new_account)
